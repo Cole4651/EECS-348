8bb55e911f29e3fed87e912cfa492ca3d55382b7
Cole4651 <133807007+Cole4651@users.noreply.github.com>
2026-09-08 16:20:33 -0500

    added some comments and created task b file

--- lab1/InsertionSortA.py
+++ lab1/InsertionSortA.py
@@ -1,5 +1,7 @@
 # Cole Nola
 # 3196815
+# 9/8/2026
+# Tuesdays 4:00pm
 # Task 1.a Insertion sort algorithm using a new array
 
 def insertion_sort_method1(arr):
---
+++ lab1/InsertionSortB.py
@@ -0,0 +1,5 @@
+# Cole Nola
+# 3196815
+# 9/8/2026
+# Tuesdays 4:00pm
+# Task 1.a Insertion sort algorithm using in place array method
